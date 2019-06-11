b73694ebc172ce345b0508d3c13e8c58f02a96e6
iRadEntertainment <devitadario@gmail.com>
2019-06-12 01:01:36 +0200

    Initial commit

---
+++ scripts/glb.gd
@@ -0,0 +1,44 @@
+#===================#
+#     GLOBAL.GD     #
+#===================#
+
+extends Node
+
+#log
+var log_max_size = 1000
+
+#network
+var fl_connected = false
+var fl_logged_in = false
+var net_id = -1
+
+#swipe_detect
+var fl_screen_p = false
+var fl_dragging = false
+var current_tab = 1
+
+
+#--------------------- console utilities ---------------------
+func log_print(string):
+	var dic = OS.get_datetime()
+	var time = [dic["month"],dic["day"],dic["hour"],dic["minute"],dic["second"]]
+	var log_entry = [time,string]
+	data_mng.console_log.push_back(log_entry)
+	while data_mng.console_log.size() > log_max_size:
+		data_mng.console_log.pop_front()
+	
+	print(format_log_entry(log_entry))
+
+func format_log_entry(log_entry):
+	var time = log_entry[0]
+	var string = log_entry[1]
+	#print sdtout
+	var time_str   = "%02d/%02d [%02d:%02d:%02d]"%time
+	var format_str = "%s - %s"%[time_str,string]
+	return (format_str)
+
+#--------------------- other utilities ---------------------
+func n_base():
+	var root=get_tree().get_root()
+	return root.get_child(root.get_child_count()-1)
+
---
+++ scripts/netwrk.gd
@@ -0,0 +1,72 @@
+#====================#
+#     netwrk.gd      #
+#====================#
+extends Node
+
+const PORT        = 5000
+const ADDRESS     = "81.4.107.146" #"127.0.0.1" #"81.4.107.146"
+
+
+
+func _ready():
+	#connect network signals
+	get_tree().multiplayer.connect("network_peer_packet",self,"_on_packets_received")
+	get_tree().connect("connected_to_server",self,"_on_connection_succeeded")
+	get_tree().connect("connection_failed",self,"_on_connection_failed")
+	
+	get_tree().connect("network_peer_connected", self, "_on_user_connected")
+	get_tree().connect("network_peer_disconnected", self, "_on_user_disconnected")
+	get_tree().connect("server_disconnected", self, "_on_server_disconnected")
+
+func connect_to():
+	var host = NetworkedMultiplayerENet.new()
+	var res = host.create_client(ADDRESS,PORT)
+	
+	if res != OK:
+		glb.log_print ("NETWORK: Impossible to create client")
+		glb.log_print (str("NETWORK: Error:",res) )
+		return
+	else:
+		glb.log_print ("NETWORK: connecting to server...")
+	get_tree().set_network_peer(host)
+	glb.net_id = get_tree().multiplayer.get_network_unique_id()
+
+func disconnect_from():
+	get_tree().set_network_peer(null)
+	glb.fl_connected = false
+
+#----------------- network signal functions ---------------------
+func _on_connection_failed():
+#	$scr/bg/lb_print.text = str($scr/bg/lb_print.text,"\n","Connection failed - ","Error: ")
+	get_tree().set_network_peer(null)
+	glb.fl_connected = false
+	glb.log_print("NETWORK: connection failed")
+	
+func _on_connection_succeeded():
+#	$scr/bg/lb_print.text = str($scr/bg/lb_print.text,"\n","Connection succeded - ")
+	glb.fl_connected = true
+	glb.log_print("NETWORK: connection succeeded")
+#	$btn_bg/btn.text = "disconnect"
+
+#----------------- network communications ---------------------
+
+func _on_packets_received(id,packet):
+	#id == 1: id = "Server"
+	glb.log_print( "NETWORK: <- %s - %s"%[ id , bytes2var(packet) ] )
+	if id == 1:
+		cmd_from_server(bytes2var(packet))
+
+func _on_user_connected(id):    print("user connected %s"%id)
+func _on_user_disconnected(id): print("user disconnected %s"%id)
+func _on_server_disconnected(): print("server offline")
+
+func send_msg_data(dat, id = 0):
+	#id == 0: all peers
+	var err = get_tree().multiplayer.send_bytes(var2bytes(dat),id)
+	if err == OK:
+		glb.log_print( "NETWORK: -> %s - %s"%[ id , dat ] )
+	else:
+		glb.log_print( "NETWORK: unable to send %s - %s\nError: %s"%[ id , dat , err] )
+
+func cmd_from_server(cmd):
+	print("NETWORK: Server commands - %s"%[cmd])
