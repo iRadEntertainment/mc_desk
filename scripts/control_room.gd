773034a3b14dec21def387e41703c2ffa4f5f827
iRadEntertainment <devitadario@gmail.com>
2019-11-24 02:27:29 +0100

    Client->Server script updates in Control Room

--- scripts/control_room.gd
+++ scripts/control_room.gd
@@ -3,4 +3,11 @@ extends Control
 const tag = "Control room"
 
 func _ready():
-	pass
+	show_panel(2)
+
+func show_panel(pnl = 0):
+	$hbox/pnl2.hide()
+	$hbox/pnl3.hide()
+	match pnl:
+		2: $hbox/pnl2.show()
+		3: $hbox/pnl3.show()
--- scripts/remote_func.gd
+++ scripts/remote_func.gd
@@ -2,17 +2,13 @@ extends Node
 
 signal server_log_received
 signal server_log_updated
-var server_log
 
-remote func receive_server_log(val):
-	server_log = val
+signal file_content_received
+
+#=================== SERVER COMMS =========================
+
+remote func receive_server_log(server_log):
 	emit_signal("server_log_received",server_log)
-	return
-	for entry in val:
-		var date = entry[0]
-		var user = entry[1]
-		var msg  = entry[2]
-		prints(user,msg)
 
 remote func receive_last_server_entry(entry):
 	emit_signal("server_log_updated",entry)
@@ -20,3 +16,5 @@ remote func receive_last_server_entry(entry):
 remote func return_user_name():
 	return glb.user_name
 
+remote func receive_file_content(content):
+	emit_signal("file_content_received",content)
